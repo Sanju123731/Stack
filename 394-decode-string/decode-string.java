2766a4b2eb83594deaebabba139aeccf18814728
Sanju <sanjumalik2006@gmail.com>
2026-05-01 07:12:14 +0530

    Time: 1 ms (83.63%) | Memory: 42.9 MB (57.52%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,41 +1,39 @@
 class Solution {
     public String decodeString(String s) {
-       Stack<Character>st=new Stack<>();
-       for(char ch:s.toCharArray())
-       {
-        if(ch!=']')
-         {
-            st.push(ch);
-         }
-         else{
-            StringBuilder str=new StringBuilder() ;
-            while(!st.isEmpty() && st.peek()!='[')
-            {
-                str.insert(0,st.pop());
-            }
-            st.pop();
-            StringBuilder num=new StringBuilder() ;
-            while(!st.isEmpty() && Character.isDigit(st.peek()))
-            {
-                num.insert(0,st.pop());
-            }
-            int repeat=Integer.parseInt(num.toString());
-            StringBuilder exp=new StringBuilder();
-            for(int i=0;i<repeat;i++)
-            {
-                exp.append(str);
-            }
-             for(char c:exp.toString().toCharArray())
-           {
-            st.push(c);
-           }
-       } 
-    }
-       StringBuilder res=new StringBuilder();
-       for(char c:st)
-       {
-        res.append(c);
+      Stack<Integer> numSt=new Stack<>();
+      Stack<StringBuilder> strSt=new Stack<>();
+      StringBuilder currStr=new StringBuilder();
+      int currNum=0;
+      for(char ch:s.toCharArray())
+      {
+        if(Character.isDigit(ch))
+        {
+            currNum=currNum*10+(ch-'0');
         }
-        return res.toString();
+        else if(ch=='[')
+        {
+            numSt.push(currNum);
+            strSt.push(currStr);
+            currNum=0;
+            currStr=new StringBuilder();
+        }
+        else if(ch==']')
+        {
+            int repeat=numSt.pop();
+
+        
+        StringBuilder prevStr=strSt.pop();
+        for(int i=0;i<repeat;i++)
+        {
+            prevStr.append(currStr);
+        }
+        currStr=prevStr;
+      }
+
+      else{
+          currStr.append(ch);
+      }
+    }
+    return currStr.toString();
 }
 }
